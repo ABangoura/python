88f330e547e978a4db49b1ed1a0fd0ea0421c621
ABangoura <albangoura@aol.com>
2024-05-21 02:34:51 -0400

    Complete exercise 3.7

---
+++ chapter-03/exercise_03_07.py
@@ -0,0 +1,7 @@
+""" Table of Squares and cubes (0-5) """
+
+print(f'{"number":>6} {"square":>6} {"cube":>6}') # Table header.
+for number in range(6):
+    # Calculate/display squares and cubes, right alligned,
+    # formatted to 6 character width.
+    print(f'{number:>6} {number ** 2:>6} {number ** 3:>6}') 
